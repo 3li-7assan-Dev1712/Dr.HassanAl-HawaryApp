04194d8ac63f2a3a86a24aa5010842b6f370f328
Ali Hassan <alihassan17122002@gmail.com>
2023-08-16 08:11:22 +0200

    created the first simple SignInScreen

--- app/src/main/java/com/example/hassanal_hawary/presentation/sign_in/SignInState.kt
+++ app/src/main/java/com/example/hassanal_hawary/presentation/sign_in/SignInState.kt
@@ -2,5 +2,6 @@ package com.example.hassanal_hawary.presentation.sign_in
 
 data class SignInState(
     val isSignInSuccessful: Boolean = false,
-    val errorMessage: String?
+    val errorMessage: String? = null
+
 )
--- app/src/main/java/com/example/hassanal_hawary/presentation/sign_in/SignInViewModel.kt
+++ app/src/main/java/com/example/hassanal_hawary/presentation/sign_in/SignInViewModel.kt
@@ -1,6 +1,27 @@
 package com.example.hassanal_hawary.presentation.sign_in
 
-class SignInViewModel
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
+class SignInViewModel(): ViewModel()
 {
+    private val _state = MutableStateFlow(SignInState())
+
+    val state = _state.asStateFlow()
+
+    fun onSignInResult(signInResult: SignInResult) {
+        _state.update {
+            it.copy(
+                isSignInSuccessful = signInResult.data != null,
+                errorMessage = signInResult.errorMessage
+            )
+        }
+    }
+    fun resetState() {
+        _state.update {
+            SignInState()
+        }
+    }
 }
