45c08301fddc5cb4d7c0157e054262c8b61392dd
Ali Hassan <alihassan17122002@gmail.com>
2023-08-22 11:12:16 +0200

    start adding email password sign in to the app

--- app/src/main/java/com/example/hassanal_hawary/presentation/sign_in/SignInScreen.kt
+++ app/src/main/java/com/example/hassanal_hawary/presentation/sign_in/SignInScreen.kt
@@ -2,12 +2,24 @@ package com.example.hassanal_hawary.presentation.sign_in
 
 import android.widget.Toast
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -29,16 +41,76 @@ fun SignInScreen(
         }
     }
 
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        EmailPasswordSection()
+
+        Spacer(modifier = Modifier.height(32.dp))
+
         Button(onClick = {
             onSignInClick()
         }) {
             Text(text = "Sign In")
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EmailPasswordSection() {
+    val emailState = rememberSaveable {
+        mutableStateOf("")
+    }
+    val passwordState = rememberSaveable {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier.padding(horizontal = 15.dp)
+    ) {
+        TextField(
+            value = emailState.value, onValueChange = {
+                emailState.value = it
+            },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true,
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "Email Section"
+                )
+            },
+            label = {
+                Text(
+                    text = "البريد الإلكتروني"
+                )
+            }
+
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
+            value = passwordState.value,
+            onValueChange = {
+                passwordState.value = it
+            },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true,
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = "Password"
+                )
+            },
+            label = {
+                Text(
+                    text = "كلمة السر"
+                )
+            })
     }
 }
