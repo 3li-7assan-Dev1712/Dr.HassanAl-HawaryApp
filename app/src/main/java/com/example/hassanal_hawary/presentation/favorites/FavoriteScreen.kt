9757ec205963050e64890a3c1973df1f049e6c27
Ali Hassan <alihassan17122002@gmail.com>
2024-05-14 23:32:38 +0200

    redesigned all articles screen

--- app/src/main/java/com/example/hassanal_hawary/presentation/favorites/FavoriteScreen.kt
+++ app/src/main/java/com/example/hassanal_hawary/presentation/favorites/FavoriteScreen.kt
@@ -1,17 +1,26 @@
 package com.example.hassanal_hawary.presentation.favorites
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.example.hassanal_hawary.R
 import com.example.hassanal_hawary.domain.model.Article
+import com.example.hassanal_hawary.presentation.all_articles.ArticleListItem
 import com.example.hassanal_hawary.presentation.main_screen.MainScreenGridItem
 import com.example.hassanal_hawary.ui.theme.Blue40
+
 
 @Composable
 fun FavoriteScreen(
@@ -37,14 +46,53 @@ fun Articles(
     LazyColumn(
         modifier = modifier
     ) {
-        /*item {
+
+        item {
             Text(
-                text = context.getString(R.string.favorite_articles),
+                text = LocalContext.current.getString(R.string.articles),
+                style = MaterialTheme.typography.headlineLarge
+            )
+        }
+
+        items(
+            count = articles.size
+        ) {
+
+            ArticleListItem(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .padding(8.dp),
+                article = articles[it],
+                articleIndex = it,
+
+                ) { articleIndex ->
+                onArticleClick(articleIndex)
+            }
+        }
+
+    }
+
+}
+
+@Composable
+fun FavoriteArticles(
+    modifier: Modifier,
+    articles: List<Article>,
+    onArticleClick: (Int) -> Unit
+) {
+
+    LazyColumn(
+        modifier = modifier
+    ) {
+        item {
+            Text(
+                text = LocalContext.current.getString(R.string.favorite_articles),
                 style = MaterialTheme.typography.headlineLarge
             )
 
             Spacer(modifier = Modifier.height(8.dp))
-        }*/
+        }
         items(
             count = articles.size
         ) {
